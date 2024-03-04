4120e6f9bf4d9fc70d3226edb96b9ed980d84ead
Ivan <ivanjose_04@hotmail.com>
2024-03-04 16:02:52 -0500

    add config.properties

--- src/main/java/org/example/MongoDBHelper.java
+++ src/main/java/org/example/MongoDBHelper.java
@@ -7,17 +7,25 @@ import com.mongodb.client.MongoDatabase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class MongoDBHelper {
     private static final Logger logger = LoggerFactory.getLogger(MongoDBHelper.class);
     private MongoDatabase database;
 
     public MongoDBHelper() {
-        try {
-            String connectionString = "mongodb+srv://ivanjocij:lnxR0jb23nTM7zVs@ecowash.hgysnkq.mongodb.net/lavadero?retryWrites=true&w=majority&appName=Ecowash";
+        Properties prop = new Properties();
+        try (InputStream input = new FileInputStream("config.properties")) {
+            prop.load(input);
+            String connectionString = prop.getProperty("mongodb.uri");
             MongoClient mongoClient = MongoClients.create(connectionString);
-            database = mongoClient.getDatabase("lavadero");
-        } catch (Exception e) {
-            logger.error("Error al conectar con la base de datos MongoDB", e);
+            // Asume que el nombre de la base de datos está incluido en la URI
+            database = mongoClient.getDatabase("lavadero"); // O extrae el nombre de la base de datos de la URI si es variable
+        } catch (IOException ex) {
+            logger.error("Error al cargar la configuración de MongoDB", ex);
         }
     }
 
